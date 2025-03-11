305e2488fee3e1f248d96e7c7f6094088487f7c9
Artur <artur.gawrych@gmail.com>
2025-03-11 17:43:09 +0000

    init asteroids

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatabe = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatabe, drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     print("Starting Asteroids!")
@@ -19,8 +22,12 @@ def main():
                 return
             
         screen.fill((0,0,0,1))
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        updatabe.update(dt)
+    
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
