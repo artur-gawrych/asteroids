e32802364017198b4316e7e788a230e1995959d0
Artur <artur.gawrych@gmail.com>
2025-03-13 12:03:18 +0000

    fixed shooting

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, radius=PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +24,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -32,8 +34,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if (keys[pygame.K_SPACE] and self.timer <= 0):
             self.shoot()
+        
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -43,3 +46,4 @@ class Player(CircleShape):
        bullet = Shoot(self.position.x, self.position.y, self.radius)
        bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation)
        bullet.velocity *= PLAYER_SHOOT_SPEED
+       self.timer = PLAYER_SHOOT_COOLDOWN
